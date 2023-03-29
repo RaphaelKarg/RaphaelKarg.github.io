c27b8600dd4d24647f67d8c6ae0ef8a6562d3e29
RaphaelKarg <rafasliakos123@gmail.com>
2023-03-30 01:46:38 +0300

    NEW VERSION UPDATE SITE 30.3.2023

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//---------------------------For the activate dropdown navigation menu and for the lines ---------------------------//
+function active() {
+    lines = document.querySelector(".dropdown-button");
+    lines.classList.toggle("dropdown-button-active");
+
+    navBar = document.querySelector(".menu-container");
+    navBar.classList.toggle("menu-container-active");
+}
+
+
+alert("Attention: This website is still under construction!\nAnyway I hope you like it :)\nHave fun! ");
